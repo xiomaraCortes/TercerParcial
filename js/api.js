8601b7741024cde057d433fde2997e0413c681e5
xiomaraCortes <55770691+xiomaraCortes@users.noreply.github.com>
2020-11-25 13:15:35 -0500

    se agrega el json con los paises

---
+++ js/api.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/*se crea la constante(restAPI) para la api, la cual esta el archivo es json 
+    se saca de la pagina https://restcountries.eu/
+*/
+
+const restAPI = "https://restcountries.eu/rest/v2/all";
+console.log(restAPI)
