8af0a440161881cc3a01803b5667174f66941ac5
xiomaraCortes <55770691+xiomaraCortes@users.noreply.github.com>
2020-11-25 17:54:24 -0500

    Se agrega cambio de color, abrir droplist

--- js/api.js
+++ js/api.js
@@ -5,4 +5,24 @@
 */
 
 const restAPI = "https://restcountries.eu/rest/v2/all";
-console.log(restAPI)
+// console.log(restAPI)
+
+// se crean las variables las cuales estan especificadas en el index 
+const countriesEl = document.getElementById("countries");
+const toggleBtn = document.getElementById("toggle");
+const filterBtn = document.getElementById("filter");
+const regionFilter = filterBtn.querySelectorAll("li");
+const searchEl = document.getElementById("search");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close");
+
+//Cambiar de color 
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+})
+
+filterBtn.addEventListener("click", () => {
+    filterBtn.classList.toggle("open");
+
+})
+
